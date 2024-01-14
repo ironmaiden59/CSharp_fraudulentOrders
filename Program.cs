2e9fbce009abafd26fc859a78e9b4e1bb3fea281
ironmaiden59 <ralphdcouto1987@gmail.com>
2024-01-13 20:41:34 -0500

    Reverse a string and count the o's

--- Program.cs
+++ Program.cs
@@ -51,24 +51,45 @@ foreach(string items in orders)
 }
 */
 
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+
+/*
 Random random = new();
 string[] orderIDs = new string[5];
-// Loop through each blank orderID
+
 for (int i = 0; i < orderIDs.Length; i++)
 {
-  // Get a random value that equates to ASCII letters A through E
   int prefixValue = random.Next(65, 70);
-  // Convert the random value into a char, then a string
   string prefix = Convert.ToChar(prefixValue).ToString();
-  // Create a random number, pad with zeroes
   string suffix = random.Next(1, 1000).ToString("000");
-  // Combine the prefix and suffix together, then assign to current OrderID
+  
   orderIDs[i] = prefix + suffix;
 }
-// Print out each orderID
+
 foreach(var orderID in orderIDs)
 {
   Console.WriteLine(orderID);
 }
+*/
+
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
+
 
 
--- obj/Debug/net8.0/Array.AssemblyInfo.cs
+++ obj/Debug/net8.0/Array.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Array")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e808bfcfb86a6a69d274c7a7c05504211cd18cdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f761cc07bca69091b9d4ae468d44c1548d452c0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Array")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Array")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
